cf7f99ed6615243d4cbb136be9260c5d7cf01546
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2024-12-17 10:34:13 +0800

    Update main.py
    Signed-off-by: Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>

--- main.py
+++ main.py
@@ -337,7 +337,7 @@ def publish_reel(page_id, page_access_token, video_id, description):
         return None
 
 video_title = text_quote
-video_description = "https://edwardize.blogspot.com/ " +  text_quote
+video_description = "https://multiculturaltoolbox.com/ " +  text_quote
 video_file_path = f"{output_dir}/{FINAL_VIDEO}"
 
 session_data = initialize_upload_session(PAGE_ID, PAGE_ACCESS_TOKEN)
@@ -454,7 +454,7 @@ def upload_video_to_youtube(video_file_path, title, description, tags, category_
 
 # Example usage for YouTube
 youtube_title = shorten(text_quote, width=90, placeholder="...")
-youtube_description = "https://edwardize.blogspot.com/ " +  text_quote
+youtube_description = "https://multiculturaltoolbox.com/ " +  text_quote
 youtube_tags = ['cats', 'facts', 'https://edwardize.blogspot.com/']
 youtube_category_id = '22'  # YouTube category ID
 youtube_privacy_status = 'public'
