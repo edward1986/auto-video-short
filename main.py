5d6e3e62579df665be0aae6ebc312f336013eb4d
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2025-02-08 17:29:28 +0800

    Update main.py
    Signed-off-by: Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google.oauth2.credentials import Credentials
-# Load environment variables from .env file if running locally
 load_dotenv(".env")
 
 # Constants and configuration
