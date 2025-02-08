4d447f1a8d9c9eaea978874464653d8f7d9c1819
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2025-02-08 10:32:57 +0800

    Update SoundCreate.py
    Signed-off-by: Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>

--- videoProcess/SoundCreate.py
+++ videoProcess/SoundCreate.py
@@ -19,7 +19,8 @@ ELEVENLABS_API_KEYS = [
     "sk_d78925238331831bfac502006c8415a5fbb052bdaa6688e9",
     "sk_ab0230e48aaf55264693c09d0cd0e5822f4f14d8851c4241",
     "sk_dfc880f060fd604e2087f3886bdace4d8b2ca74b8465a7db",
-    "sk_654a7862dda64f5a585211b7a577970bf2e398d8e30839f8"
+    "sk_654a7862dda64f5a585211b7a577970bf2e398d8e30839f8", 
+    "sk_d8b3755b444957564366a430e09c1c90c3be1b163d9f9a8d"
 ]
 
 # List of available ElevenLabs voice IDs
