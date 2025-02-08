93ee98c5ae7cda88b81c3f83f35d4c02668c698a
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2025-02-08 19:26:24 +0800

    Update main.py
    Signed-off-by: Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>

--- main.py
+++ main.py
@@ -542,7 +542,7 @@ def upload_video_to_youtube(video_file_path, title, description, tags, category_
         
         return {"error": str(e)}
 
-slug = shorten(text_quote, width=90, placeholder="...").replace('"', '').replace("Here's the polished and professional version of the blog post", '').replace('The title of the blog post is', '').replace(':', '').replace('<br>', '').replace('*', '').replace('The title of this edited blog post is', '').replace('Based on your edited blog post, I would title it', '').replace('Here is the edited blog post', '').replace('Here is the revised blog post', '').replace('The title is', '').replace('The title of this blog post is', '').replace('Here is a polished and professional version of the blog post', '')
+slug = shorten(text_quote, width=90, placeholder="").replace('"', '').replace("Here's the polished and professional version of the blog post", '').replace('The title of the blog post is', '').replace(':', '').replace('<br>', '').replace('*', '').replace('The title of this edited blog post is', '').replace('Based on your edited blog post, I would title it', '').replace('Here is the edited blog post', '').replace('Here is the revised blog post', '').replace('The title is', '').replace('The title of this blog post is', '').replace('Here is a polished and professional version of the blog post', '')
     
 youtube_title = shorten(text_quote, width=90, placeholder="...")
 youtube_description = "👉 Explore now at https://multiculturaltoolbox.com/blog/" + slug +  text_quote
