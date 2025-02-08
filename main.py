79a06d73917b620f8ccc0844d1ed6e8ab643de14
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2025-02-09 00:38:57 +0800

    Update main.py
    Signed-off-by: Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>

--- main.py
+++ main.py
@@ -551,6 +551,7 @@ slug = shorten(text_quote, width=90, placeholder="").replace('"', '').replace("H
 
 # Remove unwanted special characters and non-alphanumeric characters
 slg = re.sub(r'[^a-zA-Z0-9\s-]', '', slug.replace('The title is:', ''))
+embed = '<iframe width="560" height="315" src=f'https://www.youtube.com/embed/{response['id']}?si=29DB6WpyN3vo8Ez1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
 
 # Optionally, convert to lowercase and replace spaces with hyphens for the final slug format
 slug_final = slg.lower().replace(" ", "-")
@@ -568,7 +569,7 @@ if 'id' in response:
     print('Response:', response)
     keywords = "SEO, website, marketing, search engines"
     thumbnail = "default-thumbnail.jpg" 
-    insert_blog_post_to_db(youtube_title, shorten(text_quote, width=90, placeholder="..."), text_quote, keywords, slug,  response.get("snippet", {}).get("thumbnails", {}).get("default", {}).get("url", "No Thumbnail Found"))
+    insert_blog_post_to_db(youtube_title, shorten(text_quote, width=90, placeholder="..."), embed + "" + text_quote, keywords, slug,  response.get("snippet", {}).get("thumbnails", {}).get("default", {}).get("url", "No Thumbnail Found"))
 else:
     print('Failed to upload video to YouTube.')
     print('Response:', response)
