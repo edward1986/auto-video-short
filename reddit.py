e8e10a0e5b600d3c6146a3359092e4266bbb481e
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2025-02-22 14:16:05 +0800

    Update reddit.py
    Signed-off-by: Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>

--- reddit.py
+++ reddit.py
@@ -1,3 +1,4 @@
+print("reddit")
 import praw, os
 import requests, datetime
 from moviepy.editor import * 
@@ -11,7 +12,7 @@ username = "Current_Platform990"
 password = "eDwArD!@#1"
 
 folder = os.getcwd()    
-print("reddit")
+
 #            Get The First Posts
 reddit = praw.Reddit(client_id = client_id, client_secret = client_secret, user_agent = user_agent, username = username, password = password)
 print(calendar.weekday(int(datetime.datetime.now().strftime('%Y')), int(datetime.datetime.now().strftime('%m')), int(datetime.datetime.now().strftime('%d'))))
