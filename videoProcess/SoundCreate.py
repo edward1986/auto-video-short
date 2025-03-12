bec3d7ca4da1a140f2f7598d22c739b833e37a77
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2025-03-12 14:15:30 +0800

    Update SoundCreate.py
    Signed-off-by: Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>

--- videoProcess/SoundCreate.py
+++ videoProcess/SoundCreate.py
@@ -10,6 +10,7 @@ AUDIO = os.getenv("AUDIO_NAME", "speech.mp3")
 
 # API Keys (with fallback)
 ELEVENLABS_API_KEYS = [
+    "sk_8cbb486e47a4c86d45010bbc267ebadc1ad151cd15e70e3d",
     "sk_bb5915932d41adcbb460547ccd13b13014777d826007a0ce",
     "sk_9df605c181274d695c7c6cfe1ce0841cd04bb02fef3d1522",
     "sk_20332f0c1d8310b0400bd0dba6e732c7659dc71ab5d2941f",
