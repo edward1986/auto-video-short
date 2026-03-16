7c68f1ec30bedda76bcdba79540a2adce07f73ed
edge43 <edge@ntc.gov.ph>
2026-03-16 16:47:03 +0800

    Update main.py
    Signed-off-by: edge43 <edge@ntc.gov.ph>

--- main.py
+++ main.py
@@ -607,7 +607,8 @@ try:
         final = CompositeVideoClip([video_clip] + text_clips, size=video_clip.size)
 
     final_video_path = f"{output_dir}/{FINAL_VIDEO}"
-    final.write_videofile(final_video_path, codec="libx264", threads=8)
+    threads = min(os.cpu_count() or  4)
+    final.write_videofile(final_video_path, codec="libx264", threads=threads)
     base64_video = video_to_base64(final_video_path)
 
     # ✅ Explicitly close clips to release system resources
